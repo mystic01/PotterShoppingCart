4f91714faf27df818ecd50e9b75b54c0c4c4f73c
Mystic <mystic.lin@liteon.com>
2017-06-20 01:00:12 +0800

    第二個紅燈_第一集1本_第二集1本_預計回傳190

--- PotterShoppingCart.Tests/PotterShoppingCartTests.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTests.cs
@@ -27,5 +27,25 @@ namespace PotterShoppingCart.Tests
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void CalculateTotalPriceTest_第一集1本_第二集1本_預計回傳190()
+        {
+            //Arrange
+            const string episode1Isbn = "9573317249";
+            const string episode2Isbn = "9573317583";
+            var books = new Dictionary<string, int>()
+            {
+                { episode1Isbn, 1},
+                { episode2Isbn, 1}
+            };
+            var expected = 190;
+
+            //Act
+            var actual = PotterShoppingCart.CalculateTotalPrice(books);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
--- PotterShoppingCart/BookPriceLookupTable.cs
+++ PotterShoppingCart/BookPriceLookupTable.cs
@@ -6,7 +6,8 @@ namespace PotterShoppingCart
     {
         public static readonly Dictionary<string, int> ISBN_TO_PRICE_TABLE = new Dictionary<string, int>()
         {
-            {"9573317249", 100}//哈利波特 第一集 繁體中文版
+            {"9573317249", 100 }, //哈利波特 第一集 繁體中文版
+            {"9573317583", 100 }  //哈利波特 第二集 繁體中文版
         };
     }
 }
