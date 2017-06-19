1e4e204eefc917af04c1f57992998ad64493d451
Mystic <mystic.lin@liteon.com>
2017-06-20 00:57:06 +0800

    第一個綠燈_第一集1本_回傳100

---
+++ PotterShoppingCart/BookPriceLookupTable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PotterShoppingCart
+{
+    internal class BookPriceLookupTable
+    {
+        public static readonly Dictionary<string, int> ISBN_TO_PRICE_TABLE = new Dictionary<string, int>()
+        {
+            {"9573317249", 100}//哈利波特 第一集 繁體中文版
+        };
+    }
+}
--- PotterShoppingCart/PotterShoppingCart.cs
+++ PotterShoppingCart/PotterShoppingCart.cs
@@ -9,7 +9,14 @@ namespace PotterShoppingCart
     {
         public static int CalculateTotalPrice(Dictionary<string,int> books)
         {
-            return -1;
+            int totalPrice = 0;
+            foreach (var bookItem in books)
+            {
+                int bookPrice;
+                if (BookPriceLookupTable.ISBN_TO_PRICE_TABLE.TryGetValue(bookItem.Key, out bookPrice))
+                    totalPrice += bookPrice * bookItem.Value;
+            }
+            return totalPrice;
         }
     }
 }
