cf0ba3647f2c8985226e3668be02c978c5af988e
Mystic <mystic.lin@liteon.com>
2017-06-20 21:07:32 +0800

    第五個紅燈_第一集1本_第二集1本_第三集1本_第四集1本_第五集1本_預計回傳375

--- PotterShoppingCart.Tests/PotterShoppingCartTests.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTests.cs
@@ -93,5 +93,31 @@ namespace PotterShoppingCart.Tests
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void CalculateTotalPriceTest_第一集1本_第二集1本_第三集1本_第四集1本_第五集1本_預計回傳375()
+        {
+            //Arrange
+            const string episode1Isbn = "9573317249";
+            const string episode2Isbn = "9573317583";
+            const string episode3Isbn = "9573318008";
+            const string episode4Isbn = "9573318318";
+            const string episode5Isbn = "9573319861";
+            var books = new Dictionary<string, int>()
+            {
+                { episode1Isbn, 1},
+                { episode2Isbn, 1},
+                { episode3Isbn, 1},
+                { episode4Isbn, 1},
+                { episode5Isbn, 1},
+            };
+            var expected = 375;
+
+            //Act
+            var actual = PotterShoppingCart.CalculateTotalPrice(books);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
