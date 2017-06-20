b2d2530f61e72c9b00c7794f9d67acb4516562dc
Mystic <mystic.lin@liteon.com>
2017-06-20 20:27:55 +0800

    第三個紅燈_第一集1本_第二集1本_第三集1本_預計回傳270

--- PotterShoppingCart.Tests/PotterShoppingCartTests.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTests.cs
@@ -47,5 +47,27 @@ namespace PotterShoppingCart.Tests
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void CalculateTotalPriceTest_第一集1本_第二集1本_第三集1本_預計回傳270()
+        {
+            //Arrange
+            const string episode1Isbn = "9573317249";
+            const string episode2Isbn = "9573317583";
+            const string episode3Isbn = "9573318008";
+            var books = new Dictionary<string, int>()
+            {
+                { episode1Isbn, 1},
+                { episode2Isbn, 1},
+                { episode3Isbn, 1}
+            };
+            var expected = 270;
+
+            //Act
+            var actual = PotterShoppingCart.CalculateTotalPrice(books);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
